18f5e66b1555049c376d6c565ce6b47f1a414750
Arnav Raj <97836095+Arnavary1@users.noreply.github.com>
2025-08-19 13:41:50 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./App.css";
+import Header from "./Component/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./Component/navbar";
+import Home from "./Component/Home";
+import Footer from "./Component/Footer";
+import Headergreen from "./Component/Headergreen";
+import Navbarg from "./Component/navbargreen";
+import SellerSection from "./Component/SellerSection";
+import Homegreen from "./Component/Homegreen";
+import SustainabilityReportsSection from "./Component/Sustainability";
+import EducationSection from "./Component/Educationsection";
+import Orders from "./Component/Orders";
+import Checkout from "./Component/Checkout";
+import Thanks from "./Component/Thanks";
+import ProductDetails from "./Component/ProductDetails";
+import ProductDetails1 from "./Component/ProductDetails1";
+import Dashboard from "./Component/Dashboard";
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path="/green" element={[<Headergreen />, <Navbarg />, <Homegreen />, <Footer />]} />
+          <Route path="/" element={[<Header />, <NavBar />, <Home />, <Footer />]} />
+          <Route path="/seller" element={[<Headergreen />, <Navbarg />, <SellerSection />, <Footer />]} />
+          <Route path="/sustainability" element={[<Headergreen />, <Navbarg />, <SustainabilityReportsSection />, <Footer />]} />
+          <Route path="/education" element={[<Headergreen />, <Navbarg />, <EducationSection />, <Footer />]} />
+          <Route path="/orders" element={[<Header />, <Navbarg />, <Orders />]} />
+          <Route path="/checkout" element={[<Header />, <Checkout />, <Footer />]} />
+          <Route path="/dashboard" element={[<Header />, <Navbarg />, <Dashboard />]} />
+          <Route path="/thanks" element={[<Header />, <Thanks />]} />
+          <Route path="/product" element={[<Headergreen />, <Navbarg />, <ProductDetails />, <Footer />]} />
+          <Route path="/product1" element={[<Headergreen />, <Navbarg />, <ProductDetails1 />, <Footer />]} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
